662e12315720deca7b08e6173f78cc3f411df7b7
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-03 20:46:25 +0600

    backend fully dockerized

--- snapenhance-backend/app.py
+++ snapenhance-backend/app.py
@@ -136,4 +136,5 @@ def get_processed_image(filename):
     return send_from_directory(PROCESSED_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
