debb02d14d6003da9990d834ed1b00cdbf7f9bf9
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-23 18:01:54 +0600

    a new comment added

--- snapenhance-frontend/src/components/Gallery.jsx
+++ snapenhance-frontend/src/components/Gallery.jsx
@@ -23,7 +23,7 @@ const Gallery = () => {
     formData.append("effect", effect);
 
     const response = await fetch(
-      "https://snapenhance-backend-production.up.railway.app/upload",
+      "https://snapenhance-backend-production.up.railway.app/upload", //Backend API
       {
         method: "POST",
         body: formData,
